36e1a4d0a270ebb12b7f571653a879a94abfd2fe
eric <ericwang27759@gmail.com>
2024-01-24 22:01:57 -0800

    testing getStaticProps with Filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const HomePage = ({ products }) => (
   <ul>
     {
@@ -7,9 +10,13 @@ const HomePage = ({ products }) => (
 );
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product 1' }]
+      products: data.products
     }
   };
 }
